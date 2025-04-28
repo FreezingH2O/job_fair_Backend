5e4661182ef78763ac188918e582402c8007339e
FreezingH2O <airadajitauleeruk@gmail.com>
2025-04-28 14:10:55 +0700

    fix docker

--- server.js
+++ server.js
@@ -67,10 +67,9 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
     PORT, 
-    console.log(
-        "Server running in",
-        process.env.NODE_ENV,
-        "on" + `${process.env.HOST} mode on port ${PORT}`)
+    '0.0.0.0', 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+
     );
 
 const swaggerOptions ={
